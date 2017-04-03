cbb5fec511f71f89700fdf4317f384b52def1516
Aaron <aagavin@gmail.com>
2017-04-02 20:36:24 -0400

    added ablity to get episodes for a tv show

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,44 @@ let express = require('express');
 let router = express.Router();
 let imdb = require('imdb-api')
 
-
+// main route can be used to 
+// wake up heroku
 router.get('/', (req, res) => {
-  res.status(200).json({success: true});
+  res.status(200).json({ success: true });
 });
 
-router.get('/id/:id', (req, res)=>{
+// get movie/tv info by id
+router.get('/id/:id', (req, res) => {
   let id = req.params.id;
-  imdb.getById(id).then(things =>{
-    res.status(200).json(things)
-  }).catch(err =>{
-    res.status(200).json(err);
+  imdb.getById(id).then(things => {
+    res.status(200).json(things);
+  }).catch(err => {
+    res.status(400).json(err);
   });
 });
 
-router.get('/name/:name', (req, res)=>{
+// get episodes of of tv show by id 
+router.get('/id/:id/episodes', (req, res) => {
+  let id = req.params.id;
+  imdb.getById(id).then(things => {
+    if (things['_episodes']) {
+      things.episodes((err, episodes) => {
+        if (err) res.status(200).json(err);
+        res.status(200).json(episodes);
+      }).catch(err => {
+        res.status(400).json(err);
+      });
+    }
+  });
+});
+
+// get movie/tv info by name
+router.get('/name/:name', (req, res) => {
   let name = req.params.name;
-  imdb.get(name).then(things =>{
+  imdb.get(name).then(things => {
     res.status(200).json(things);
   }).catch(err => {
-    res.status(200).json(err);
+    res.status(400).json(err);
   })
 });
 
